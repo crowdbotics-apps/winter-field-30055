807abb624c0c6487404cd989bccbaff4b7ce31b6
Crowdbotics <team@crowdbotics.com>
2021-08-23 21:26:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "winter_field_30055Identifier",
+  url: "https://winter_field_30055Identifier.botics.co",
+  api: "https://winter_field_30055Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
